500b733cbd52ed0211c73bc044b54eedc6777197
MyRubyMine321 <MyRubyMine321@gmail.com>
2017-06-12 13:16:18 +0300

    update bundler

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20161106145947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.datetime "cteated_at"
@@ -32,7 +24,15 @@ ActiveRecord::Schema.define(version: 20161106145947) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
